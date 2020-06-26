2fe52f258ba37ccc2008c72b332c326b85b20dfb
Ilya Shurmanov <h82st8@gmail.com>
2020-06-26 03:54:39 +0300

    исправляет раздел т.о.

---
+++ js/script.js
@@ -0,0 +1,3 @@
+document.querySelector(".technical_support_service__button").addEventListener("click", function () {
+	document.querySelector(".technical_support_service__list").classList.toggle("technical_support_service__list--hidden");
+});
